568b6f8235bc23d2d4c3a819ea3481e7bf8ad7e6
Roy <roymatero@gmail.com>
2021-08-03 08:11:44 +0300

    dio

---
+++ lib/app/modules/explore/providers/librivox_provider.dart
@@ -0,0 +1,10 @@
+// import 'package:di';
+
+class LibrivoxProvider {
+  static final BASEURL = "https://librivox.org/api";
+  Dio dio = Dio();
+
+  Future getCategories() async {}
+
+  Future getFeed() async {}
+}
